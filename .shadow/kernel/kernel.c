e9699bb91c4bcd20ab1802dc255361caef1885a0
tracer-nju <tracer@nju.edu.cn>
2023-03-13 16:06:36 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:06:36 up  5:36,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -56,10 +56,6 @@ void splash() {
 int main(const char *args) {
   ioe_init();
 
-  puts("mainargs = \"");
-  puts(args);  // make run mainargs=xxx
-  puts("\"\n");
-
   splash();
 
   puts("Press any key to see its key code...\n");

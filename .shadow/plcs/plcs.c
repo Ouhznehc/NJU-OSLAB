a729dec3c949cae606a20ceabcaa0647ed0d8289
tracer-nju <tracer@nju.edu.cn>
2023-03-21 19:57:55 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:57:55 up  7:22,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -73,7 +73,6 @@ int main(int argc, char *argv[]) {
   N = strlen(A);
   M = strlen(B);
   T = !argv[1] ? 1 : atoi(argv[1]);
-  if(N > M) swap(N, M);
   // Add preprocessing code here
 
   for (int i = 0; i < T; i++) {

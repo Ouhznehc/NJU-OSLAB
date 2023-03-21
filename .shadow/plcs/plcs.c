76daaffee729737fcbf73b47f7cad603db9db930
tracer-nju <tracer@nju.edu.cn>
2023-03-21 17:42:16 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:42:16 up  5:07,  0 users,  load average: 0.01, 0.01, 0.00

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -14,24 +14,36 @@ mutex_t lk = MUTEX_INIT();
 cond_t thread = COND_INIT();
 cond_t global = COND_INIT();
 int consent[MAXN][MAXN];
-int global_x, gobal_y;
+int global_x, global_y;
+int round_cnt;
 
 #define DP(x, y) (((x) >= 0 && (y) >= 0) ? dp[x][y] : 0)
 #define MAX(x, y) (((x) > (y)) ? (x) : (y))
+#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 #define MAX3(x, y, z) MAX(MAX(x, y), z)
-#define COND consent[global_x][gobal_y]
+#define THREAD_COND consent[global_x][global_y]
+#define GLOBAL_COND round_cnt
 
 
 void Tworker(int id) {
   int thread_x, thread_y;
   while(1){
     mutex_lock(&lk);
-    while(!COND)
+    while(!THREAD_COND)
       cond_wait(&thread, &lk);
-    
+    thread_x = global_x;
+    thread_y = global_y;
+    consent[global_x][global_y] = 0;
+    consent[global_x - 1][global_y + 1] = 1;
+    global_x--; global_y++;
     cond_broadcast(&thread);
     mutex_unlock(&lk);
-
+    int skip_a = DP(thread_x - 1, thread_y);
+    int skip_b = DP(thread_x, thread_y - 1);
+    int take_both = DP(thread_x - 1, thread_y - 1) + (A[thread_x] == B[thread_y]);
+    dp[thread_x][thread_y] = MAX3(skip_a, skip_b, take_both);
+    round_cnt--;
+    if(!round_cnt) cond_broadcast(&global);
   }
 
 }
@@ -49,7 +61,21 @@ int main(int argc, char *argv[]) {
     create(Tworker); 
   }
   for(int round = 0; round < N + M - 1; round++){
-
+    mutex_lock(&lk);
+    while(!GLOBAL_COND)
+      cond_wait(&global, &lk);
+    if(round < N){
+      global_x = round; 
+      global_y = 0;
+    }
+    else{
+      global_x = N - 1;
+      global_y = round - N + 1;
+    }
+    round_cnt = MIN(global_x, M - global_y);
+    consent[global_x][global_y] = 1;
+    cond_broadcast(&thread);
+    mutex_unlock(&lk);
   }
 
   join();  // Wait for all workers

5ad86687f21d08b7293b1c8ee9854e40321ec62f
tracer-nju <tracer@nju.edu.cn>
2023-03-28 22:36:14 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:36:14 up 1 day, 21:08,  0 users,  load average: 0.30, 0.42, 0.20

--- .shadow/plcs/plcs.c
+++ .shadow/plcs/plcs.c
@@ -12,7 +12,7 @@ short dp[MAXN][MAXN];
 
 #if __DEBUG__MODE_
   FILE *fp;
-  #define debug(...) printf(__VA_ARGS__)
+  #define debug(...) fprintf(fp, __VA_ARGS__)
 #else
   #define debug(...)  
 #endif
@@ -30,11 +30,13 @@ mutex_t lk = MUTEX_INIT();
 typedef struct info{
   short row, l, r;
 }Work;
+#define STEP 1024
 Work workload[MAXN];
 bool kill_signal;
 bool busy[MAXN];
 bool inqueue[MAXN];
-short debug_dp[MAXN][MAXN][MAXN];
+short progress[MAXN];
+
 // queue API
 short head, tail;
 Work queue[MAXN << 1];
@@ -42,8 +44,21 @@ Work queue[MAXN << 1];
 bool queue_size(){
   return head != tail;
 }
+void print_queue(){
+  short my_head = head;
+  short my_tail = tail;
+  while(my_head != my_tail){
+    debug("{%d %d %d} ", queue[my_head].row, queue[my_head].l, queue[my_head].r);
+    if (++my_head == MAXN << 1) my_head = 0;
+  }
+  debug("\n");
+}
 bool queue_push(Work work){
-  if(inqueue[work.row] || busy[work.row]) return false;
+  if(inqueue[work.row] || busy[work.row]){
+    //debug("{%d %d %d} push failed\n", work.row, work.l, work.r);
+    return false;
+  }
+  inqueue[work.row] = true;
   queue[tail] = work;
   if (++tail == MAXN << 1) tail = 0;
   return true;
@@ -56,46 +71,67 @@ Work queue_pop(){
 }
 
 
-
 #define WORKER_COND (queue_size() || kill_signal)
 
 void allocate_work(Work work){
-  short len = MIN(1024, M - work.r);
+  short bound = work.row ? progress[work.row - 1] : M;
+  Work right = (Work){.row = work.row, .l = work.r, .r = MIN(bound, work.r + STEP)};
+  Work down  = (Work){.row = work.row + 1, .l = progress[work.row + 1], .r = MIN(M, progress[work.row + 1] + STEP)};
   if(work.row == N - 1 && work.r == M) return;
-  else if(work.row == N - 1) queue_push((Work){work.row, work.r, work.r + len});
-  else if(work.r == M) queue_push((Work){work.row + 1, work.l, work.r});
+  else if(work.row == N - 1){
+    //debug("work {%d %d %d} allocate {%d %d %d}\n", work.row, work.l, work.r, right.row, right.l, right.r);
+    if(right.l < right.r)
+      queue_push(right);
+  }
+  else if(work.r == M){
+    //debug("work {%d %d %d} allocate {%d %d %d}\n", work.row, work.l, work.r, down.row, down.l, down.r);
+    if(down.l < down.r)
+      queue_push(down);
+  } 
   else {
-    queue_push((Work){work.row, work.r, work.r + len});
-    queue_push((Work){work.row + 1, work.l, work.r});
+    //debug("work {%d %d %d} allocate {%d %d %d}\n", work.row, work.l, work.r, right.row, right.l, right.r);
+    //debug("work {%d %d %d} allocate {%d %d %d}\n", work.row, work.l, work.r, down.row, down.l, down.r);
+    if(right.l < right.r)
+    queue_push(right);
+    if(down.l < down.r)
+    queue_push(down);
   }
   return;
 }
-
+void check(){
+  for(int i = 0; i < N - 1; i++){
+    if(progress[i] < progress[i + 1]){
+      debug("%d: %d fuck %d: %d\n", i, progress[i], i + 1, progress[i + 1]);
+      assert(0);
+    }
+  }
+}
 
 void Tworker(int id) {
   Work thread_work;
   while(1){
     mutex_lock(&lk);
     while(!WORKER_COND){
-      debug("worker %d sleep \n", id);
+      //debug("worker %d sleep \n", id);
       cond_wait(&cv, &lk);
-      debug("worker %d awake \n", id);
+      //debug("worker %d awake \n", id);
     }
-    debug("worker %d locked\n", id);
+    //debug("worker %d locked\n", id);
     if(kill_signal){
-      debug("worker %d killed\n", id);
-      debug("worker %d unlock\n", id);
+      //debug("worker %d killed\n", id);
+      //debug("worker %d unlock\n", id);
       cond_broadcast(&cv);
       mutex_unlock(&lk);
       break;
     }
     thread_work = queue_pop();
+    //print_queue();
     busy[thread_work.row] = true;
     if(thread_work.row == N - 1 && thread_work.r == M) {
       kill_signal = 1;
-      debug("kill signal is send by worker %d\n", id);
+      //debug("kill signal is send by worker %d\n", id);
     }
-    debug("worker %d unlock: row = %d, l = %d, r = %d\n", id, thread_work.row, thread_work.l, thread_work.r);
+    //debug("worker %d unlock: {%d %d %d}\n", id, thread_work.row, thread_work.l, thread_work.r);
     mutex_unlock(&lk);
 
     short i = thread_work.row;
@@ -108,16 +144,19 @@ void Tworker(int id) {
 
     mutex_lock(&lk);
     busy[thread_work.row] = false;
+    progress[thread_work.row] = thread_work.r;
     allocate_work(thread_work);
     cond_broadcast(&cv);
-    debug("worker %d finished\n", id);
+    //debug("worker %d finished\n", id);
+    //print_queue();
+    //check();
     mutex_unlock(&lk);
   }
 }
 
 int main(int argc, char *argv[]) {
   #ifdef __DEBUG__MODE_
-    fp = fopen("log.txt", "w");
+    fp = freopen("log.txt", "w", stderr);
     setbuf(stdout, NULL);
   #endif
 
@@ -129,7 +168,7 @@ int main(int argc, char *argv[]) {
   // Add preprocessing code here
 
 
-  queue_push((Work){0, 0, MIN(1024, M)});
+  queue_push((Work){0, 0, MIN(STEP, M)});
   for (int i = 0; i < T; i++) {
     create(Tworker); 
   }

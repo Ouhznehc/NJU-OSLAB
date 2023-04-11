9cff8d6e5f218b94cfcefca7b1452df74af62e7a
tracer-nju <tracer@nju.edu.cn>
2023-04-11 22:59:34 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:59:34 up 5 days, 3 min,  0 users,  load average: 0.13, 0.21, 0.15

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -12,19 +12,78 @@ static void os_init() {
   pmm->init();
 }    
 
-int a[20] = {1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8 KB, 32 KB, 512 KB, 1 MB, 2 MB, 8 MB, 16 MB};
+spinlock_t lk[100005], plock;
+uintptr_t alloc[100005];
 static void os_run() {
-  for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
-    putch(*s == '*' ? '0' + cpu_current() : *s);
+  int now = cpu_current();
+  // int s = 0, maxx = 0;
+  while (1)
+  {
+    int pos = (rand() * rand()) % 10000;
+    // int pos = s++;
+    // if (pos == 100000)
+    //   break;
+    spin_lock(&lk[pos]);
+    if (alloc[pos] == 0)
+    {
+      int size = rand() % 20;
+      if (size < 16)
+        size = 1 << (size % 10);
+      else if (size != 19)
+        size = 4096;
+      // else
+      // size = 4096;
+      // spin_lock(&plock);
+      // printf("cpu %d want to alloc time %d with %dB\n", now, pos, size);
+      // spin_unlock(&plock);
+
+      alloc[pos] = (uintptr_t)pmm->alloc(size);
+      if (alloc[pos] == 0)
+      {
+        spin_lock(&plock);
+        printf("no more space\n");
+        spin_unlock(&plock);
+        spin_unlock(&lk[pos]);
+        // s = 0;
+        // maxx = pos;
+        continue;
+      }
+      spin_lock(&plock);
+      printf("cpu %d alloc at %p with %dB\n", now, alloc[pos], size);
+      spin_unlock(&plock);
+    }
+    else
+    {
+      pmm->free((void *)alloc[pos]);
+      spin_lock(&plock);
+      printf("cpu %d free time %d at %p\n", now, pos, alloc[pos]);
+      spin_unlock(&plock);
+      alloc[pos] = 0;
+      // if (pos == maxx - 1)
+      // {
+      //   s = 0;
+      //   spin_unlock(&lk[pos]);
+      //   continue;
+      // }
+    }
+    spin_unlock(&lk[pos]);
   }
-  //int num = 10;
-  while(1){
-    void *test = pmm->alloc(a[rand() % 20]);
-    if(test != NULL) pmm->free(test);
-    else Log("alloc failed");
+
+  uintptr_t al[4005];
+  for (int i = 1; i <= 4000; i++)
+  {
+    al[i] = (uintptr_t)pmm->alloc(2);
+    printf("%p\n", al[i]);
   }
-  while (1) ;
-}                     
+  printf("\n");
+  for (int i = 1; i <= 9; i++)
+  {
+    pmm->free((void *)al[i]);
+    printf("%p\n", (uintptr_t)pmm->alloc(1024));
+  // }
+  while(1);
+  }
+}                    
         
 MODULE_DEF(os) = {
   .init = os_init,

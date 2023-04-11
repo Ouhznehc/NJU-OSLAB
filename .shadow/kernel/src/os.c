d58f982c46467d56473c92727ccb7e31b241c6c5
tracer-nju <tracer@nju.edu.cn>
2023-04-11 16:30:22 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:30:22 up 4 days, 17:34,  0 users,  load average: 0.13, 0.04, 0.01

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -16,7 +16,7 @@ static void os_run() {
   for (const char *s = "Hello World from CPU #*\n"; *s; s++) {
     putch(*s == '*' ? '0' + cpu_current() : *s);
   }
-  pmm->alloc(1023);
+  pmm->alloc(2);
   while (1) ;
 }                     
         

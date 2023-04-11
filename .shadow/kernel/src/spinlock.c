bb771dc017197134d9d2b3387ef0cd50e2d30363
tracer-nju <tracer@nju.edu.cn>
2023-04-11 18:07:16 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:07:16 up 4 days, 19:11,  0 users,  load average: 1.76, 0.60, 0.22

--- .shadow/kernel/src/spinlock.c
+++ .shadow/kernel/src/spinlock.c
@@ -12,14 +12,14 @@ void init_lock(spinlock_t *lk, char *name){
 }
 
 void spin_lock(spinlock_t *lk) {
-  if(holding(lk)) kernal_panic("spin_lock: %s", lk->name);
+  // if(holding(lk)) kernal_panic("spin_lock: %s", lk->name);
   while(atomic_xchg(&lk->locked, 1) != 0);
   __sync_synchronize();
   lk->cpu = cpu_current();
 }
 
 void spin_unlock(spinlock_t *lk) {
-  if(!holding(lk)) kernal_panic("spin_unlock: %s", lk->name);
+  // if(!holding(lk)) kernal_panic("spin_unlock: %s", lk->name);
   lk->cpu = -1;
   __sync_synchronize();
   atomic_xchg(&lk->locked, 0);

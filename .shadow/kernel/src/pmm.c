abdd5be3ce24072d32bc88a3cadc5c41cc9612d3
tracer-nju <tracer@nju.edu.cn>
2023-04-11 18:04:28 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:04:28 up 4 days, 19:08,  0 users,  load average: 0.00, 0.01, 0.02

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -81,6 +81,7 @@ static void kfree(void *ptr) {
 }
 
 static void pmm_init() {
+  init_lock(&heap_lock, "heap_lock");
   heap_start = heap.start;
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);

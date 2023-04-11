1697db030693ea28c93872dfa42328d24919e742
tracer-nju <tracer@nju.edu.cn>
2023-04-11 22:01:08 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:01:08 up 4 days, 23:05,  0 users,  load average: 0.03, 0.06, 0.01

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -1,8 +1,8 @@
 #include <common.h>
 
+static spinlock_t heap_lock;
 static page_t *heap_start = NULL;
 static kmem_cache kmem[MAX_CPU];
-static spinlock_t heap_lock;
 int slab_type[SLAB_TYPE] = {2, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096};
 
 static inline int match_slab_type(size_t size)

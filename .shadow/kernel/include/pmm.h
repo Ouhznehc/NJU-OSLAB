4d4ede05b484d29a0514f7116bf372d556e72ec3
tracer-nju <tracer@nju.edu.cn>
2023-04-11 16:00:45 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:00:45 up 4 days, 17:04,  0 users,  load average: 0.00, 0.00, 0.00

--- .shadow/kernel/include/pmm.h
+++ .shadow/kernel/include/pmm.h
@@ -1,4 +1,20 @@
 #ifndef __PMM_H__
 #define __PMM_H__
 
+#define KB * 1024
+#define MB KB * 1024
+
+typedef struct list_t
+{
+  struct list_t *next;
+}list_t;
+
+typedef struct kmem_cache
+{
+  int cpu;
+  spinlock_t lk;
+
+}kmem_cache;
+
+
 #endif

591268a675a937e3035b9bee5736dd2fe7c37fd9
tracer-nju <tracer@nju.edu.cn>
2023-04-11 20:09:12 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:09:12 up 4 days, 21:13,  0 users,  load average: 0.01, 0.01, 0.07

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -124,6 +124,7 @@ static void pmm_init() {
       kmem[i].slab_list[j].next = &pages->node;
       kmem[i].free_list[j].next = &pages->node;
       kmem[i].free_page[j] = SLAB_MIN;
+      Log("fuck union: page=%07p, node=%07p", (size_t)&pages, (size_t)&(pages->node));
       Assert(((size_t)&pages == (size_t)&(pages->node)), "fuck union: page=%07p, node=%07p", (size_t)&pages, (size_t)&(pages->node));
     }
 

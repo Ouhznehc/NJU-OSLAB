1eef63a46a72ba7210ecd3e89d2e8711cdce14ed
tracer-nju <tracer@nju.edu.cn>
2023-04-12 22:18:04 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:18:04 up 5 days, 23:22,  0 users,  load average: 0.01, 0.05, 0.06

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -402,7 +402,9 @@ static void pmm_init()
 
   uintptr_t pmsize = ((uintptr_t)heap.end - (uintptr_t)heap.start);
   printf("Got %d MiB heap: [%p, %p)\n", pmsize >> 20, heap.start, heap.end);
-
+#ifndef DOUBLE_PMM
+  memset(heap.start, 0, pmsize);
+#endif
   memory_t *heap_start = (memory_t *)(heap.start);
   heap_start->next = NULL;
   heap_start->memory_start = (void *)((uintptr_t)heap_start + MEMORY_CONFIG);

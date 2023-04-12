5c38818fe31661cf09675cd0c6a73cd5f01b9580
tracer-nju <tracer@nju.edu.cn>
2023-04-13 02:07:19 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:07:19 up 6 days,  3:11,  0 users,  load average: 0.00, 0.11, 0.19

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -79,6 +79,7 @@ static void os_run()
     al[i] = (uintptr_t)pmm->alloc(2);
     Log("%d times: %p\n", i, al[i]);
   }
+  Log("finish");
   for (int i = 1; i <= 9; i++)
   {
     pmm->free((void *)al[i]);

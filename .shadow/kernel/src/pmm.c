3d06f12c9e0aba3a512b0d656dfafef463ed369d
tracer-nju <tracer@nju.edu.cn>
2023-04-12 20:26:18 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:26:18 up 5 days, 21:30,  0 users,  load average: 0.00, 0.02, 0.00

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -196,7 +196,7 @@ static void *kmalloc_slab(size_t size)
     ret = object_from_slab(page);
   else
   {
-    Assert(cpu == 0, "cpu = %d", cpu);
+    Assert(cpu_current() == 0, "cpu = %d", cpu);
     Assert(kmem[cpu].slab_list[slab_index] != NULL, "kmem[cpu].slab_list[slab_index] == NULL: cpu=%d, slab=%d", cpu, slab_type[slab_index]);
     page = kmem[cpu].slab_list[slab_index]->next;
     assert(page->object_counter <= page->object_capacity);

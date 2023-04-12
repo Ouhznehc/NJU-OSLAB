5ff525936f048d144ce6cc60f5aec7677ac16b99
tracer-nju <tracer@nju.edu.cn>
2023-04-13 00:17:17 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:17:17 up 6 days,  1:21,  0 users,  load average: 0.45, 0.20, 0.07

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -25,7 +25,7 @@ static void os_run()
   int pos = 0;
   while (1)
   {
-    pos = (pos + 1) % 300;
+    pos = (pos + 1) % 400;
     Log("pos %d", pos);
     spin_lock(&lk[pos]);
     if (alloc[pos] == 0)

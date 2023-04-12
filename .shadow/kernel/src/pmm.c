d9c5473cd8110ff1dfc0d61bc8a4b848824878f1
tracer-nju <tracer@nju.edu.cn>
2023-04-13 01:36:51 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:36:51 up 6 days,  2:41,  0 users,  load average: 0.07, 0.08, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -225,6 +225,8 @@ static void *kalloc_page()
   if (ret == NULL)
     return NULL;
   assert(ret != NULL);
+  assert(*(uintptr_t *)(ret->memory_start - sizeof(uintptr_t)) == MAGIC);
+  assert(*(uintptr_t *)(ret->memory_start - sizeof(uintptr_t)) == (uintptr_t)ret);
   assert((uintptr_t)ret->memory_start + ret->memory_size - (uintptr_t)ret == 8 KB);
   assert(ret->memory_size == 4 KB);
   Log("kalloc_page end");

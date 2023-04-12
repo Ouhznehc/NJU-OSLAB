3de96991a67321d9dcb014ed6c47fd2944b0aace
tracer-nju <tracer@nju.edu.cn>
2023-04-12 21:17:39 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:17:39 up 5 days, 22:21,  0 users,  load average: 0.41, 0.21, 0.07

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -7,7 +7,7 @@
 #include "spinlock.h"
 #include "pmm.h"
 
-#define __DEBUG_MODE__
+// #define __DEBUG_MODE__
 
 #ifdef assert
 #undef assert
@@ -21,64 +21,71 @@
 
 #ifdef __DEBUG_MODE__
 
+extern spinlock_t debug_lk;
 
-    extern spinlock_t debug_lk;
+#define debug(format, ...)                         \
+    printf("\33[1;35m[%s: %d] " format "\33[0m\n", \
+           __func__, __LINE__, ##__VA_ARGS__);
 
-    #define debug(format, ...) \
+#define kernal_panic(format, ...)                               \
+    do                                                          \
+    {                                                           \
+        debug("\33[1;31msystem panic: " format, ##__VA_ARGS__); \
+        halt(1);                                                \
+    } while (0)
+
+#define Log(format, ...)                               \
+    do                                                 \
+    {                                                  \
+        spin_lock(&debug_lk);                          \
         printf("\33[1;35m[%s: %d] " format "\33[0m\n", \
-            __func__, __LINE__, ## __VA_ARGS__);
+               __func__, __LINE__, ##__VA_ARGS__);     \
+        spin_unlock(&debug_lk);                        \
+    } while (0)
 
-    #define kernal_panic(format, ...) \
-        do { \
-            debug("\33[1;31msystem panic: " format, ## __VA_ARGS__); \
-            halt(1); \
-        } while (0)
+#define panic(format, ...)                                    \
+    do                                                        \
+    {                                                         \
+        Log("\33[1;31msystem panic: " format, ##__VA_ARGS__); \
+        halt(1);                                              \
+    } while (0)
 
-    #define Log(format, ...) \
-        do{ \
-        spin_lock(&debug_lk); \
-        printf("\33[1;35m[%s: %d] " format "\33[0m\n", \
-            __func__, __LINE__, ## __VA_ARGS__); \
-        spin_unlock(&debug_lk); \
-        }while(0)
+#define panic_on(cond, format, ...) \
+    do                              \
+    {                               \
+        if (cond)                   \
+        {                           \
+            panic(format);          \
+        }                           \
+    } while (0)
 
-    #define panic(format, ...) \
-        do { \
-            Log("\33[1;31msystem panic: " format, ## __VA_ARGS__); \
-            halt(1); \
-        } while (0)
-
-    #define panic_on(cond,format,...) \
-        do{ \
-            if(cond) { \
-            panic(format); \
-            }  \
-        }while(0)
-
-    #define assert(cond) \
-        do { \
-            if (!(cond)) { \
+#define assert(cond)                              \
+    do                                            \
+    {                                             \
+        if (!(cond))                              \
+        {                                         \
             panic("Assertion failed: %s", #cond); \
-            } \
-        } while (0)
+        }                                         \
+    } while (0)
 
-    #define Assert(cond,format,...) \
-        do{ \
-            if(!(cond)) { \
-            panic(format); \
-            }  \
-        }while(0)
+#define Assert(cond, format, ...) \
+    do                            \
+    {                             \
+        if (!(cond))              \
+        {                         \
+            panic(format);        \
+        }                         \
+    } while (0)
 
-    #define TODO() panic("please implement me")
-
+#define TODO() panic("please implement me")
 
 #else
-    #define kernal_panic(format, ...)
-    #define Log(format,...)           
-    #define panic(format, ...)        
-    #define panic_on(cond,format,...) 
-    #define assert(cond)              
-    #define Assert(cond,format,...)   
+#define kernal_panic(format, ...)
+#define Log(format, ...)
+#define panic(format, ...)
+#define panic_on(cond, format, ...)
+#define assert(cond)
+#define Assert(cond, format, ...)
 #endif
 
 #endif

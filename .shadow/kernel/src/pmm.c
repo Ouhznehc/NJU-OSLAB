be38ae546b521f2446cc649e96eb323a3c096576
tracer-nju <tracer@nju.edu.cn>
2023-04-12 22:44:30 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:44:30 up 5 days, 23:48,  0 users,  load average: 0.03, 0.08, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -345,7 +345,7 @@ static void kfree_slab(slab_t *page, void *ptr)
   assert(getbit(page->bitset[i], j) == 1);
   clrbit(page->bitset[i], j);
 #ifdef DOUBLE_PMM
-  uintptr_t *check = (page->object_start + (32 * i + j) * page->object_size);
+  uintptr_t *check = (uintptr_t *)((uintptr_t)page->object_start + (32 * i + j) * page->object_size);
   assert(*check == MAGIC);
   *check = 0;
 #endif

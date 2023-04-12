ff829b66e2b72d0e5d3a7fc4685b4fecd808585d
tracer-nju <tracer@nju.edu.cn>
2023-04-12 19:59:45 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:59:45 up 5 days, 21:03,  0 users,  load average: 0.21, 0.10, 0.03

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -291,12 +291,13 @@ static void slab_init()
 {
   for (int cpu = 0; cpu < cpu_count(); cpu++)
   {
-    Log("end initial");
-    assert(0);
+
     init_lock(&kmem[cpu].lk, "cpu");
 
     for (int slab = 0; slab < SLAB_TYPE; slab++)
     {
+      Log("end initial");
+      assert(0);
       fetch_page_to_slab(slab, cpu);
       kmem[cpu].free_slab[slab] = SLAB_MIN;
     }

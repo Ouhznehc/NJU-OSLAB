5c96364d75eca839cce7bd93a2019a78d5a0d0e9
tracer-nju <tracer@nju.edu.cn>
2023-04-12 22:50:34 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:50:34 up 5 days, 23:54,  0 users,  load average: 0.08, 0.08, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -53,15 +53,15 @@ static void *object_from_slab(slab_t *page)
         page->object_counter++;
         ret = page->object_start + (32 * i + j) * page->object_size;
 #ifdef DOUBLE_PMM
-        uintptr_t *check = ret;
-        assert(page != NULL);
-        assert((uintptr_t)check < (uintptr_t)heap.end && (uintptr_t)check > (uintptr_t)heap.start);
-        assert(((uintptr_t)page + 8 KB) < (uintptr_t)heap.end);
-        if (page->object_size >= 4)
-        {
-          assert(*check == 0);
-          *check = MAGIC;
-        }
+        // uintptr_t *check = ret;
+        // assert(page != NULL);
+        // assert((uintptr_t)check < (uintptr_t)heap.end && (uintptr_t)check > (uintptr_t)heap.start);
+        // assert(((uintptr_t)page + 8 KB) < (uintptr_t)heap.end);
+        // if (page->object_size >= 4)
+        // {
+        //   assert(*check == 0);
+        //   *check = MAGIC;
+        // }
 #endif
         return ret;
       }
@@ -347,10 +347,10 @@ static void kfree_slab(slab_t *page, void *ptr)
   assert(getbit(page->bitset[i], j) == 1);
   clrbit(page->bitset[i], j);
 #ifdef DOUBLE_PMM
-  uintptr_t *check = (uintptr_t *)((uintptr_t)page->object_start + (32 * i + j) * page->object_size);
-  assert((uintptr_t)check < (uintptr_t)heap.end && (uintptr_t)check > (uintptr_t)heap.start);
-  assert(*check == MAGIC);
-  *check = 0;
+  // uintptr_t *check = (uintptr_t *)((uintptr_t)page->object_start + (32 * i + j) * page->object_size);
+  // assert((uintptr_t)check < (uintptr_t)heap.end && (uintptr_t)check > (uintptr_t)heap.start);
+  // assert(*check == MAGIC);
+  // *check = 0;
 #endif
   if (page->object_counter == 0)
   {

f594ba61098be9605938afa88d7deef003528ca7
Ouhznehc <OuhznehcPersonal@gmail.com>
2023-04-13 22:40:44 +0800

    format code

--- kernel/src/spinlock.c
+++ kernel/src/spinlock.c
@@ -1,20 +1,17 @@
 #include <common.h>
 
-bool holding(spinlock_t *lk)
-{
+bool holding(spinlock_t* lk) {
   return lk->locked && lk->cpu == cpu_current();
   // return lk->locked;
 }
 
-void init_lock(spinlock_t *lk, char *name)
-{
+void init_lock(spinlock_t* lk, char* name) {
   lk->name = name;
   lk->locked = 0;
   lk->cpu = -1;
 }
 
-void spin_lock(spinlock_t *lk)
-{
+void spin_lock(spinlock_t* lk) {
   if (holding(lk))
     kernal_panic("spin_lock: %s at CPU #&d", lk->name, lk->cpu);
   while (atomic_xchg(&lk->locked, 1) != 0)
@@ -23,8 +20,7 @@ void spin_lock(spinlock_t *lk)
   lk->cpu = cpu_current();
 }
 
-void spin_unlock(spinlock_t *lk)
-{
+void spin_unlock(spinlock_t* lk) {
   if (!holding(lk))
     kernal_panic("spin_unlock: %s at CPU #%d", lk->name, lk->cpu);
   lk->cpu = -1;

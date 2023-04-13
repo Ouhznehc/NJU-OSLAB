9acccabc322c291a10b0ea7b57e37e33207bd8fb
tracer-nju <tracer@nju.edu.cn>
2023-04-13 11:44:40 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:44:40 up 1 min,  0 users,  load average: 0.04, 0.03, 0.01

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -23,70 +23,75 @@ static void os_run()
   {
     init_lock(&lk[i], "lk");
   }
-  int now = cpu_current();
-  while (1)
+  // int now = cpu_current();
+  // while (1)
+  // {
+  //   int pos = (rand() * rand()) % 10000;
+  //   Log("");
+  //   spin_lock(&lk[pos]);
+  //   if (alloc[pos] == 0)
+  //   {
+  //     int size = rand() % 20;
+  //     if (size < 16)
+  //       size = 1 << (size % 10 + 13);
+  //     else if (size != 19)
+  //       size = 4096;
+  //     alloc[pos] = (uintptr_t)pmm->alloc(size);
+  //     if (alloc[pos] == 0)
+  //     {
+  //       Log("no more space\n");
+  //       spin_unlock(&lk[pos]);
+  //       continue;
+  //     }
+  //     else
+  //     {
+  //       assert(size >= 4);
+  //       Log("CPU #%d alloc at %07p with pos = %d, size = %d", now, alloc[pos], pos, size);
+  //       for (int i = 0; i < size / 4; i++)
+  //       {
+  //         int *check = (int *)(alloc[pos] + 4 * i);
+  //         if (*check == -1)
+  //           panic("double alloc");
+  //       }
+  //       num[pos] = size;
+  //       memset((void *)alloc[pos], -1, size);
+  //     }
+  //   }
+  //   else
+  //   {
+  //     assert(num[pos] >= 4);
+  //     pmm->free((void *)alloc[pos]);
+  //     for (int i = 0; i < num[pos] / 4; i++)
+  //     {
+  //       int *check = (int *)(alloc[pos] + 4 * i);
+  //       if (*check == 0)
+  //         panic("double free");
+  //     }
+  //     memset((void *)alloc[pos], 0, num[pos]);
+  //     Log("CPU #%d free at %p with pos = %d, size = %d", now, alloc[pos], pos, num[pos]);
+  //     alloc[pos] = 0;
+  //     num[pos] = 0;
+  //   }
+  //   spin_unlock(&lk[pos]);
+  // }
+  int cnt = 1;
+  while (pmm->alloc(2) != NULL)
   {
-    int pos = (rand() * rand()) % 10000;
-    Log("");
-    spin_lock(&lk[pos]);
-    if (alloc[pos] == 0)
-    {
-      int size = rand() % 20;
-      if (size < 16)
-        size = 1 << (size % 10 + 13);
-      else if (size != 19)
-        size = 4096;
-      alloc[pos] = (uintptr_t)pmm->alloc(size);
-      if (alloc[pos] == 0)
-      {
-        Log("no more space\n");
-        spin_unlock(&lk[pos]);
-        continue;
-      }
-      else
-      {
-        assert(size >= 4);
-        Log("CPU #%d alloc at %07p with pos = %d, size = %d", now, alloc[pos], pos, size);
-        for (int i = 0; i < size / 4; i++)
-        {
-          int *check = (int *)(alloc[pos] + 4 * i);
-          if (*check == -1)
-            panic("double alloc");
-        }
-        num[pos] = size;
-        memset((void *)alloc[pos], -1, size);
-      }
-    }
-    else
-    {
-      assert(num[pos] >= 4);
-      pmm->free((void *)alloc[pos]);
-      for (int i = 0; i < num[pos] / 4; i++)
-      {
-        int *check = (int *)(alloc[pos] + 4 * i);
-        if (*check == 0)
-          panic("double free");
-      }
-      memset((void *)alloc[pos], 0, num[pos]);
-      Log("CPU #%d free at %p with pos = %d, size = %d", now, alloc[pos], pos, num[pos]);
-      alloc[pos] = 0;
-      num[pos] = 0;
-    }
-    spin_unlock(&lk[pos]);
+    Log("cnt = %d", cnt++);
   }
 
-  uintptr_t al[4005];
-  for (int i = 1; i <= 4000; i++)
-  {
-    al[i] = (uintptr_t)pmm->alloc(2);
-    Log("%d times: %p\n", i, al[i]);
-  }
-  for (int i = 1; i <= 1000; i++)
-  {
-    pmm->free((void *)al[i]);
-    void *ret = pmm->alloc(4096);
-    Log("%d times: %p\n", i, ret);
-  }
+  // uintptr_t al[4005];
+  // for (int i = 1; i <= 4000; i++)
+  // {
+  //   al[i] = (uintptr_t)pmm->alloc(2);
+  //   Log("%d times: %p\n", i, al[i]);
+  // }
+  // for (int i = 1; i <= 1000; i++)
+  // {
+  //   pmm->free((void *)al[i]);
+  //   void *ret = pmm->alloc(4096);
+  //   Log("%d times: %p\n", i, ret);
+  // }
 #endif
 
   while (1)

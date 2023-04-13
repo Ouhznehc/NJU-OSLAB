543cdbbacb1a071f22968eb1b4b357c8ac80b06d
tracer-nju <tracer@nju.edu.cn>
2023-04-13 13:59:58 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:59:58 up  2:17,  0 users,  load average: 0.33, 0.10, 0.07

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -172,6 +172,7 @@ static void page_to_slab_pool(memory_t *page)
   assert(page->next == NULL);
   page->next = slab_pool.next;
   slab_pool.next = page;
+  Log("free %07p", page->memory_start);
   spin_unlock(&slab_lock);
 }
 

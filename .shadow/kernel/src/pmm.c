5619406bc2fce190713620661d4a7887ff0dc771
tracer-nju <tracer@nju.edu.cn>
2023-04-13 13:28:25 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:28:25 up  1:45,  0 users,  load average: 0.04, 0.10, 0.04

--- .shadow/kernel/src/pmm.c
+++ .shadow/kernel/src/pmm.c
@@ -368,6 +368,7 @@ static inline void *fetch_header(void *ptr, int magic)
 
 static void kfree(void *ptr)
 {
+  spin_lock(&test_lock);
   int magic = fetch_magic(ptr);
   if (magic == MAGIC)
   {
@@ -379,6 +380,7 @@ static void kfree(void *ptr)
     slab_t *page = fetch_header(ptr, magic);
     return kfree_slab(page, ptr);
   }
+  spin_unlock(&test_lock);
 }
 
 static void slab_init()

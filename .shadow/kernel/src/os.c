07a4dcedc93120ab3d14361466b46b9e8fc4ea6a
tracer-nju <tracer@nju.edu.cn>
2023-04-13 18:21:33 +0800

    Linux DESKTOP-F995BJS 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:21:33 up  6:38,  0 users,  load average: 0.10, 0.04, 0.01

--- .shadow/kernel/src/os.c
+++ .shadow/kernel/src/os.c
@@ -87,7 +87,7 @@ static void os_run()
     {
       // Log("\nos try alloc %dB", 8);
       // Log("begin alloc %d", i);
-      test[i] = pmm->alloc(2);
+      test[i] = pmm->alloc(4096);
       if (test[i] == NULL)
       {
         Log("cnt=%d", cnt);

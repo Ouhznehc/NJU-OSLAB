e48d70c7686df55c2e067095d134d3a5e4cd988c
tracer-nju <tracer@nju.edu.cn>
2023-04-24 02:09:43 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:09:43 up  8:58,  0 users,  load average: 0.07, 0.06, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -54,8 +54,8 @@ void display_sperf() {
   qsort(syscalls, syscall_count, sizeof(syscall_t), syscall_compare);
   printf("%d\n", syscall_count);
   for (int i = 0; i < syscall_count; i++) {
-    int ratio = (int)((syscalls[i].time / total_time) * 100)
-      printf("%s (%d%%)\n", syscalls[i].name, ratio);
+    int ratio = (int)((syscalls[i].time / total_time) * 100);
+    printf("%s (%d%%)\n", syscalls[i].name, ratio);
   }
   fflush(stdout);
 }

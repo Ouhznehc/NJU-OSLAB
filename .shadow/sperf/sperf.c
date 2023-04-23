44242e68dd90b08839178a304da31a570ce898a8
tracer-nju <tracer@nju.edu.cn>
2023-04-24 01:13:44 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:13:44 up  8:02,  0 users,  load average: 0.15, 0.07, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -113,7 +113,7 @@ int main(int argc, char* argv[]) {
   pid_t pid = fork();
   if (pid == 0) {
     close(pipefd[0]);
-    if (dup2(pipefd[1], STDOUT_FILENO) == -1) {
+    if (dup2(pipefd[1], STDERR_FILENO) == -1) {
       perror("dup2");
       exit(EXIT_FAILURE);
     }

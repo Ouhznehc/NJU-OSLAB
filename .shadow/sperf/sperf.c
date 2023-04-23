a6040ff514272aa20e614b0023edf41d9e8847e8
tracer-nju <tracer@nju.edu.cn>
2023-04-23 23:43:05 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:43:05 up  6:32,  0 users,  load average: 0.01, 0.02, 0.00

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -3,8 +3,13 @@
 #include <unistd.h>
 #include <string.h>
 
-//! syscall_t
+#define MAX_PATHS 100
+#define MAX_ARGVS 100
+#define MAX_FILENAME 64
 #define MAX_SYSCALL 100
+#define MAX_BUFFER 512
+
+//! syscall_t
 typedef struct syscall_t {
   char name[64];
   double time;
@@ -12,34 +17,100 @@ typedef struct syscall_t {
 int syscall_compare(const void* a, const void* b) {
   const syscall_t* syscallA = (const syscall_t*)a;
   const syscall_t* syscallB = (const syscall_t*)b;
-  return syscallA->time < syscallB->time;
+  return (syscallA->time > syscallB->time) - (syscallA->time < syscallB->time);
 }
 
 syscall_t syscalls[MAX_SYSCALL];
-
+int syscall_count;
 
 //! path_env
-#define MAX_PATHS 100
-char* paths[MAX_PATHS];
+char* envp[MAX_PATHS];
+char* args[MAX_ARGVS];
 
 void fetch_path_env() {
   char* path_env = getenv("PATH");
   char* path = strtok(path_env, ":");
   int path_count = 0;
   while (path != NULL && path_count < MAX_PATHS) {
-    paths[path_count] = path;
+    envp[path_count] = path;
     path = strtok(NULL, ":");
     path_count++;
   }
-  paths[path_count] = NULL;
 }
 
+void fetch_strace_info(int fd) {
+  char buffer[MAX_BUFFER];
+  FILE* pipe_stream = fdopen(fd, "r");
+  while (fgets(buffer, MAX_BUFFER, pipe_stream) != NULL) {
+    char syscall_name[64];
+    double time;
+    if (sscanf(buffer, "%63[^'(]%*[^(](%*[^<]<%lf>)", syscall_name, &time) == 2) {
+      int exist = 0;
+      for (int i = 0; i < syscall_count; i++) {
+        if (strcmp(syscalls[i].name, syscall_name) == 0) {
+          syscalls[i].time += time;
+          exist = 1;
+        }
+      }
+      if (!exist) {
+        strcpy(syscalls[syscall_count].name, syscall_name);
+        syscalls[syscall_count].time = time;
+        syscall_count++;
+      }
+    }
+  }
+  fclose(pipe_stream);
+}
 
+char* fetch_command(char* name) {
+  if (name[0] == '/') return name;
+  for (int i = 0; envp[i]; i++) {
+    char file_path[MAX_FILENAME] = snprintf(file_path, sizeof(file_path), "%s/%s", envp[i], name);
+    if (access(file_path, F_OK) == 0)
+      return file_path;
+  }
+  perror(name);
+  exit(EXIT_FAILURE);
+}
 
+void fetch_strace_argv(int argc, char* argv[]) {
+  fetch_path_env();
+  args[0] = fetch_command("strace");
+  args[1] = "-T";
+  args[2] = fetch_command(argv[0]);
+  for (int i = 1; i < argc; i++) args[i + 2] = argv[i];
+}
 
+void display_sperf() {
+  qsort(syscalls, syscall_count, sizeof(syscall_t), syscall_compare);
+  for (int i = 0; i < syscall_count; i++) {
+    printf("%s : %lf\n", syscalls[i].name, syscalls[i].time);
+  }
+}
 
 int main(int argc, char* argv[]) {
-
-  fetch_path_env();
-
+  int pipefd[2];
+  if (pipe(pipefd) != 0) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+  pid_t pid = fork();
+  if (pid == 0) {
+    close(pipefd[0]);
+    if (dup2(pipefd[1], STDOUT_FILENO) != 0) {
+      perror("dup2");
+      exit(EXIT_FAILURE);
+    }
+    close(pipefd[1]);
+    handle_commands(argc, argv);
+    execve(args[0], args, envp);
+    perror("execve");
+    exit(EXIT_FAILURE);
+  }
+  else {
+    close(pipefd[1]);
+    fetch_strace_info(pipefd[0]);
+    close(pipefd[0]);
+    display_sperf();
+  }
 }

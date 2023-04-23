751c86131447803d4ecd64e1a0bc0ebf8df7e1ae
tracer-nju <tracer@nju.edu.cn>
2023-04-23 21:57:57 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:57:57 up  4:47,  0 users,  load average: 0.06, 0.03, 0.02

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -4,27 +4,27 @@
 #include <string.h>
 #define MAX_PATHS 100
 int main(int argc, char* argv[]) {
-  char* path_env = getenv("PATH");
-  char* path = strtok(path_env, ":");
-  char* paths[MAX_PATHS]; // 存储路径的数组
-  int path_count = 0; // 计数器
+  // char* path_env = getenv("PATH");
+  // char* path = strtok(path_env, ":");
+  // char* paths[MAX_PATHS]; // 存储路径的数组
+  // int path_count = 0; // 计数器
 
-  // 将每个路径存储到数组中
-  while (path != NULL && path_count < MAX_PATHS) {
-    paths[path_count] = path;
-    path = strtok(NULL, ":");
-    path_count++;
-  }
+  // // 将每个路径存储到数组中
+  // while (path != NULL && path_count < MAX_PATHS) {
+  //   paths[path_count] = path;
+  //   path = strtok(NULL, ":");
+  //   path_count++;
+  // }
 
-  // 输出每个路径
-  for (int i = 0; i < path_count; i++) {
-    printf("%s\n", paths[i]);
-  }
-  // char* exec_argv[] = { "strace", "ls", NULL, };
-  // char* exec_envp[] = { "PATH=/bin", NULL, };
-  // execve("strace", exec_argv, exec_envp);
+  // // 输出每个路径
+  // for (int i = 0; i < path_count; i++) {
+  //   printf("%s\n", paths[i]);
+  // }
+  char* exec_argv[] = { "strace", "ls", NULL, };
+  char* exec_envp[] = { "PATH=/bin", NULL, };
+  execve("strace", exec_argv, exec_envp);
   // execve("/bin/strace", exec_argv, exec_envp);
   // execve("/usr/bin/strace", exec_argv, exec_envp);
-  // perror(argv[0]);
+  perror(argv[0]);
   exit(EXIT_FAILURE);
 }

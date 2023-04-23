50fad48f55b0eb58aba74c3eac6839c583f43c0f
tracer-nju <tracer@nju.edu.cn>
2023-04-24 01:23:15 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:23:15 up  8:12,  0 users,  load average: 0.07, 0.06, 0.01

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -124,8 +124,8 @@ int main(int argc, char* argv[]) {
     }
     fflush(stdout);
     fclose(stdout);
-    execve("/bin/strace", exec_argv, exec_envp);
-    // execve(args[0], args, NULL);
+    // execve("/bin/strace", exec_argv, exec_envp);
+    execve(args[0], args, NULL);
     perror("execve");
     exit(EXIT_FAILURE);
   }

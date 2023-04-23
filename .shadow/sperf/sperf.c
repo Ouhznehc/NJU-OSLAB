86c2b6df16276933e69c12adf37c0e4fba9a94ca
tracer-nju <tracer@nju.edu.cn>
2023-04-23 22:15:57 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:15:57 up  5:05,  0 users,  load average: 0.00, 0.02, 0.00

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -27,11 +27,11 @@ int main(int argc, char* argv[]) {
     printf("%s\n", paths[i]);
     i++;
   }
-  char* exec_argv[] = { "strace", "ls", NULL, };
-  char* exec_envp[] = { "PATH=/bin", NULL, };
-  // execve("strace", exec_argv, exec_envp);
-  // execve("/bin/strace", exec_argv, exec_envp);
-  execve("/usr/bin/strace", exec_argv, exec_envp);
+  // char* exec_argv[] = { "strace", "ls", NULL, };
+  // char* exec_envp[] = { "PATH=/bin", NULL, };
+  // // execve("strace", exec_argv, exec_envp);
+  // // execve("/bin/strace", exec_argv, exec_envp);
+  // execve("/usr/bin/strace", exec_argv, exec_envp);
   perror(argv[0]);
   exit(EXIT_FAILURE);
 }

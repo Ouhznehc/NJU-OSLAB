7ba234cfac6ef6c54c0aed42cf1350a64feadbc6
tracer-nju <tracer@nju.edu.cn>
2023-05-05 13:08:16 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:08:16 up  1:08,  0 users,  load average: 0.02, 0.03, 0.00

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -85,7 +85,7 @@ void fetch_strace_info(int fd, int pid) {
         //exit(EXIT_FAILURE);
       }
 
-      if (regexec(&regex, buffer, 3, matches, 0) != 0) {
+      if (regexec(&regex, buffer, 2, matches, 0) != 0) {
         fprintf(stderr, "No match found\n");
         //exit(EXIT_FAILURE);
       }

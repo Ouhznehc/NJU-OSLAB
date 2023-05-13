de3b25e18af4512595e06bac1de3abdf5f693035
tracer-nju <tracer@nju.edu.cn>
2023-05-13 23:46:17 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:46:17 up 1 day,  7:38,  0 users,  load average: 0.18, 0.07, 0.02

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -160,8 +160,8 @@ int main(int argc, char* argv[]) {
   }
   else {
     int child_ret;
-    wait(&child_ret);
-    printf("%d\n", child_ret);
+    int pidd = wait(&child_ret);
+    printf("%d %d %d\n", pidd, pid, child_ret);
     // exit(child_ret);
     close(pipefd[1]);
     fetch_strace_info(pipefd[0], pid);

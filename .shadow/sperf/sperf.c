fcddfb63eba9f4fc75854dc5c2a61a88c4986783
tracer-nju <tracer@nju.edu.cn>
2023-05-14 00:25:22 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:25:22 up 1 day,  8:17,  0 users,  load average: 0.19, 0.10, 0.03

--- .shadow/sperf/sperf.c
+++ .shadow/sperf/sperf.c
@@ -113,8 +113,7 @@ void fetch_strace_info(int fd, int pid) {
 char* fetch_command(char* name) {
   static int counter = -1;
   counter++;
-  if (!name)
-    return NULL;
+  if (!name) return NULL;
   if (name[0] == '/') return name;
   for (int i = 0; env_path[i]; i++) {
     snprintf(file_path[counter], sizeof(file_path[counter]), "%s/%s", env_path[i], name);

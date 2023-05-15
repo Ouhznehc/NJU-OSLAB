0f55e6b1e3d0d876496a753b972340f8396d0712
tracer-nju <tracer@nju.edu.cn>
2023-05-15 21:34:31 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:34:31 up 3 days,  5:26,  0 users,  load average: 0.14, 0.06, 0.01

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -1,11 +1,39 @@
-#include <kernel.h>
 #include <klib.h>
+#include <os.h>
+#include <common.h>
 
+#define P kmt->sem_wait
+#define V kmt->sem_signal
+sem_t empty, fill;
+void Tproduce(void* arg) {
+  while (1) {
+    P(&empty);
+    putch('(');
+    V(&fill);
+  }
+}
+void Tconsume(void* arg) {
+  while (1) {
+    P(&fill);
+    putch(')');
+    V(&empty);
+  }
+}
+static void create_threads() {
+  kmt->sem_init(&empty, "empty", 1);
+  kmt->sem_init(&fill, "fill", 0);
+  for (int i = 0; i < 1; i++) {
+    kmt->create(pmm->alloc(sizeof(task_t)), "producer", Tproduce, NULL);
+  }
+  for (int i = 0; i < 1; i++) {
+    kmt->create(pmm->alloc(sizeof(task_t)), "consumer", Tconsume, NULL);
+  }
+}
 int main() {
   ioe_init();
   cte_init(os->trap);
   os->init();
-
+  create_threads();
   mpe_init(os->run);
   return 1;
 }

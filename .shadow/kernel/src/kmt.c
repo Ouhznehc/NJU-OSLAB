bf51e7c7849fb029ea3dbf4f763e4e8948138fdb
tracer-nju <tracer@nju.edu.cn>
2023-05-16 01:30:05 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:30:05 up 3 days,  9:22,  0 users,  load average: 0.06, 0.06, 0.03

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -126,7 +126,7 @@ static void task_list_delete(task_t* delete_task) {
 
 static task_t* task_list_query(int cpu) {
   for (task_t* cur = current_task[cpu]->next; cur != current_task[cpu]; cur = cur->next) {
-    Log("cur->status = %d", cur->status);
+    printf("cur->status = %d\n", cur->status);
   }
   for (task_t* cur = current_task[cpu]->next; cur != current_task[cpu]; cur = cur->next) {
     if (cur->status == RUNNABLE) return cur;

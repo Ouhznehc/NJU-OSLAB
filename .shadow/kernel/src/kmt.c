5def4cfb951d64ed1866bc3245e752ed782e5287
tracer-nju <tracer@nju.edu.cn>
2023-05-16 01:20:23 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:20:23 up 3 days,  9:12,  0 users,  load average: 0.25, 0.10, 0.03

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -142,8 +142,8 @@ static Context* kmt_context_save(Event ev, Context* context) {
 static Context* kmt_schedule(Event ev, Context* context) {
   int cpu = cpu_current();
   Context* ret = NULL;
-  kmt_spin_lock(&os_trap_lk);
   assert(0);
+  kmt_spin_lock(&os_trap_lk);
   if (buffer_task[cpu] != NULL) {
     Assert(buffer_task[cpu]->status == RUNNING, "buffer_task not RUNNING");
     buffer_task[cpu]->status = RUNNABLE;

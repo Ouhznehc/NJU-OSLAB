e3915a2092534892438d6d77c46f3394e35f888d
tracer-nju <tracer@nju.edu.cn>
2023-05-15 22:43:48 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:43:48 up 3 days,  6:35,  0 users,  load average: 0.39, 0.13, 0.09

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -118,13 +118,13 @@ static void task_list_init() {
 }
 
 static void task_list_insert(task_t* insert_task) {
-  kmt_spin_lock(&os_trap_lk);
+  //kmt_spin_lock(&os_trap_lk);
   Log("fuck");
   insert_task->next = task_list_head->next;
   insert_task->prev = task_list_head;
   task_list_head->next->prev = insert_task;
   task_list_head->next = insert_task;
-  kmt_spin_unlock(&os_trap_lk);
+  // kmt_spin_unlock(&os_trap_lk);
 }
 
 static void task_list_delete(task_t* delete_task) {

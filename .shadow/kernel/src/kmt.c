624b1e02c1915da111dcb2088284b7e7522a4b3f
tracer-nju <tracer@nju.edu.cn>
2023-05-15 22:48:30 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:48:30 up 3 days,  6:40,  0 users,  load average: 0.13, 0.13, 0.10

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -81,12 +81,12 @@ static void kmt_sem_init(sem_t* sem, const char* name, int value) {
 static void kmt_sem_wait(sem_t* sem) {
   kmt_spin_lock(&sem->lk);
   Log("%s try P with count = %d", sem->name, sem->count);
-  Assert(sem->count >= 0, "kmt_sem_wait: sem->count < 0");
+  // Assert(sem->count >= 0, "kmt_sem_wait: sem->count < 0");
   int is_wait = 0;
   while (sem->count == 0) {
     is_wait = 1;
     kmt_spin_unlock(&sem->lk);
-    Log("%s P failed: yield()", sem->name);
+    // Log("%s P failed: yield()", sem->name);
     yield();
   }
   if (is_wait == 1) kmt_spin_lock(&sem->lk);
@@ -97,7 +97,7 @@ static void kmt_sem_wait(sem_t* sem) {
 static void kmt_sem_signal(sem_t* sem) {
   kmt_spin_lock(&sem->lk);
   Log("%s try V with count = %d", sem->name, sem->count);
-  Assert(sem->count >= 0, "kmt_sem_signal: sem->count < 0");
+  // Assert(sem->count >= 0, "kmt_sem_signal: sem->count < 0");
   sem->count++;
   kmt_spin_unlock(&sem->lk);
 }

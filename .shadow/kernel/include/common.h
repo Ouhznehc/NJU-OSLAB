a012d8d8ff95d70dabe4aed059d00e90ece98ed0
tracer-nju <tracer@nju.edu.cn>
2023-05-26 14:42:16 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:42:16 up 26 min,  0 users,  load average: 0.00, 0.02, 0.00

--- .shadow/kernel/include/common.h
+++ .shadow/kernel/include/common.h
@@ -37,11 +37,19 @@ extern kspinlock_t debug_lk;
 #define Log(format, ...)                           \
     do                                             \
     {                                              \
-        kspin_lock(&debug_lk);                      \
         printf("[%s: %d] " format "\n",            \
                __func__, __LINE__, ##__VA_ARGS__); \
-        kspin_unlock(&debug_lk);                    \
     } while (0)
+/*
+// #define Log(format, ...)                           \
+//     do                                             \
+//     {                                              \
+//         kspin_lock(&debug_lk);                      \
+//         printf("[%s: %d] " format "\n",            \
+//                __func__, __LINE__, ##__VA_ARGS__); \
+//         kspin_unlock(&debug_lk);                    \
+//     } while (0)
+*/
 
 #define panic(format, ...)                           \
     do                                               \

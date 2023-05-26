14d865cacd6edc4cbb8ad91ceffeb9eaa33065eb
tracer-nju <tracer@nju.edu.cn>
2023-05-26 15:29:36 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:29:36 up  1:14,  0 users,  load average: 0.05, 0.02, 0.00

--- .shadow/kernel/framework/main.c
+++ .shadow/kernel/framework/main.c
@@ -29,9 +29,11 @@ static void create_threads() {
   Log("head = %d, tail = %d", runnable_head, runnable_tail);
   for (int i = 0; i < 10; i++) {
     kmt->create(pmm->alloc(sizeof(task_t)), "producer", Tproduce, NULL);
+    Log("TASK#%p : rip = %p", runnable_task[runnable_tail - 1]->stack, runnable_task[runnable_tail - 1]->context->rip);
   }
   for (int i = 0; i < 1; i++) {
     kmt->create(pmm->alloc(sizeof(task_t)), "consumer", Tconsume, NULL);
+    Log("TASK#%p : rip = %p", runnable_task[runnable_tail - 1]->stack, runnable_task[runnable_tail - 1]->context->rip);
   }
   Log("head = %d, tail = %d", runnable_head, runnable_tail);
   for (int i = runnable_head; i < runnable_tail; i++) {

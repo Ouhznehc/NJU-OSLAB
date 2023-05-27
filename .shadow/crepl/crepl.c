1a5d34590cc0e0dc19c8786744c4be5293802e70
tracer-nju <tracer@nju.edu.cn>
2023-05-27 21:05:28 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:05:28 up  2:49,  0 users,  load average: 0.05, 0.05, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -34,7 +34,7 @@ static int compile_new_lib(FILE* lib_file, char* code) {
   while (fgets(string, sizeof(string), lib_file) != NULL) puts(string);
   pid_t pid = fork();
   if (pid == 0) {
-    fclose(stderr);
+    // fclose(stderr);
     execlp("gcc", "gcc", "-shared", "-fPIC", compile_filename, "-o", "/tmp/compile.so", NULL);
     exit(1);
   }

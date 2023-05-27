9e918b137dbfa2e32d239a381a875c6e31c388f0
tracer-nju <tracer@nju.edu.cn>
2023-05-27 19:31:33 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:31:33 up  1:15,  0 users,  load average: 0.02, 0.02, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(int argc, char *argv[]) {
-  static char line[4096];
+const char crepl_filename[] = "/tmp/crepl.so";
+const char compile_filename[] = "/tmp/compile.so";
+static char line[4096];
+
+int main(int argc, char* argv[]) {
+  int fd = mkstemp(crepl_filename);
+
   while (1) {
     printf("crepl> ");
     fflush(stdout);
-    if (!fgets(line, sizeof(line), stdin)) {
-      break;
+    if (fgets(line, sizeof(line), stdin) != NULL) {
+      // if (strncmp(line, "int", 3) == 0) ;
+      0;
     }
     printf("Got %zu chars.\n", strlen(line)); // ??
   }
+
+  close(fd);
+  unlink(crepl_filename);
+  return 0;
 }

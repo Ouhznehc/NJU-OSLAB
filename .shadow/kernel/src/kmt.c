8cc32341022dcba65d0bea70f95e68e2a23615d9
tracer-nju <tracer@nju.edu.cn>
2023-05-27 15:51:32 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:51:32 up  2:05,  0 users,  load average: 0.20, 0.07, 0.03

--- .shadow/kernel/src/kmt.c
+++ .shadow/kernel/src/kmt.c
@@ -178,7 +178,7 @@ static int kmt_create(task_t* task, const char* name, void (*entry)(void* arg), 
     Log("TASK#%p : rip#%p = %p", runnable_task[j]->stack, &runnable_task[j]->context->rip, runnable_task[j]->context->rip);
   }
   task->stack = pmm->alloc(STACK_SIZE);
-  assert(*(uintptr_t*)((uintptr_t)task->stack - sizeof(uintptr_t)) == MAGIC);
+  // assert(*(uintptr_t*)((uintptr_t)task->stack - sizeof(uintptr_t)) == MAGIC);
 
   Log("after 8KB");
   for (int j = runnable_head; j < runnable_tail; j++) {

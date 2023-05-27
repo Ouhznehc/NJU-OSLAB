2350f12254b3c1d6ddb203721a6bcc17091049e5
tracer-nju <tracer@nju.edu.cn>
2023-05-27 21:26:01 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:26:01 up  3:10,  0 users,  load average: 0.07, 0.03, 0.01

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -71,7 +71,7 @@ static void update_shared_lib(char* code) {
 static int compile_shared_function(char* code) {
   strcpy(compile_filename, "/tmp/compile_XXXXXXX.c");
   compile_fd = mkstemps(compile_filename, 2);
-  compile_file = fdopen(compile_fd, "w");
+  compile_file = fdopen(compile_fd, "w+");
   copy_shared_lib(crepl_file, compile_file);
   int ret = compile_new_lib(compile_file, code);
   if (ret) update_shared_lib(code);

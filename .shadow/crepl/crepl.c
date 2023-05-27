8e3b96b499c16cb49a00c23163b4dd636d8e8bd7
tracer-nju <tracer@nju.edu.cn>
2023-05-27 21:06:24 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:06:24 up  2:50,  0 users,  load average: 0.02, 0.03, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -85,10 +85,10 @@ static int fetch_expression_value(char* expression) {
 
 int main(int argc, char* argv[]) {
   crepl_fd = mkstemp(crepl_filename);
-  crepl_file = fdopen(crepl_fd, "rw+");
+  crepl_file = fdopen(crepl_fd, "w+");
 
   compile_fd = mkstemp(compile_filename);
-  compile_file = fdopen(compile_fd, "rw+");
+  compile_file = fdopen(compile_fd, "w+");
 
   while (1) {
     printf("crepl> ");

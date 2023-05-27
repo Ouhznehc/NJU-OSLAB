e71e349b6e0d17d635971884ba1002f94892341a
tracer-nju <tracer@nju.edu.cn>
2023-05-27 19:37:33 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:37:33 up  1:21,  0 users,  load average: 0.08, 0.04, 0.01

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -9,6 +9,16 @@
 static char crepl_filename[] = "/tmp/crepl.so";
 static char compile_filename[] = "/tmp/compile.so";
 static char line[4096];
+static int is_expression;
+static int rc;
+
+static int compile_shared_lib(char* line) {
+  return 1;
+}
+
+static int fetch_expression_value(char* line) {
+  return 0;
+}
 
 int main(int argc, char* argv[]) {
   int fd = mkstemp(crepl_filename);
@@ -17,10 +27,11 @@ int main(int argc, char* argv[]) {
     printf("crepl> ");
     fflush(stdout);
     if (fgets(line, sizeof(line), stdin) != NULL) {
-      // if (strncmp(line, "int", 3) == 0) ;
-      0;
+      if (strncmp(line, "int", 3) == 0) rc = compile_shared_lib(line);
+      else rc = fetch_expression_value(line);
     }
-    printf("Got %zu chars.\n", strlen(line)); // ??
+    if (is_expression) printf("= %d\n", rc);
+    else printf("Compile %s.\n", rc ? "OK" : "error");
   }
 
   close(fd);

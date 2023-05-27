d7bb5a3ca0d188868d36c5eee4bf21fb4fc76e87
tracer-nju <tracer@nju.edu.cn>
2023-05-27 22:04:23 +0800

    Linux Ouhznehc-Windows 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:04:23 up  3:48,  0 users,  load average: 0.07, 0.04, 0.00

--- .shadow/crepl/crepl.c
+++ .shadow/crepl/crepl.c
@@ -11,6 +11,8 @@
 
 static char line[4096];
 static int is_expression;
+static int expression_cnt;
+static int is_valid;
 static int rc;
 
 static char crepl_filename[] = "/tmp/crepl_XXXXXXX.c";
@@ -21,6 +23,7 @@ static int crepl_fd;
 
 static FILE* compile_file;
 static FILE* crepl_file;
+
 
 static void copy_shared_lib(char* src_filename, char* dst_filename) {
   FILE* src_file = fopen(src_filename, "r");
@@ -89,9 +92,52 @@ static int compile_shared_lib(char* code) {
   return ret;
 }
 
+static int compile_expression(char* expression) {
+  strcpy(compile_filename, "/tmp/compile_XXXXXXX.c");
+  compile_fd = mkstemps(compile_filename, 2);
+  copy_shared_lib(crepl_filename, compile_filename);
+  int ret = compile_expression_with_lib(compile_filename, expression);
+  return ret;
+}
+
+static int compile_expression_with_lib(char* lib_filename, char* expression) {
+  FILE* lib_file = fopen(lib_filename, "a");
+  assert(lib_file != NULL);
+  fprintf(lib_file, "int expression_%d(){\n", expression_cnt);
+  fprintf(lib_file, "return %s;\n", expression);
+  fprintf(lib_file, "}\n");
+  fclose(lib_file);
+
+  pid_t pid = fork();
+  if (pid == 0) {
+    fclose(stderr);
+    execlp("gcc", "gcc", "-shared", "-fPIC", lib_filename, "-o", "/tmp/compile.so", NULL);
+    exit(1);
+  }
+  else {
+    int status;
+    waitpid(pid, &status, 0);
+    if (WIFEXITED(status) && WEXITSTATUS(status) == 0) return 1;
+    else return 0;
+  }
+}
+
+static int fetch_expression_value_with_lib() {
+  void* handle = dlopen("/tmp/compile.so", RTLD_LAZY);
+  typedef int (*expression_func)();
+  char function_name[128];
+  sprintf(function_name, "expression_%d", expression_cnt++);
+  expression_func function = (expression_func)dlsym(handle, function_name);
+  int ret = function();
+  dlclose(handle);
+  return ret;
+}
+
 static int fetch_expression_value(char* expression) {
   is_expression = 1;
-  return 0;
+  is_valid = compile_expression(expression);
+  if (!is_valid) return 0;
+  else return fetch_expression_value_with_lib();
 }
 
 int main(int argc, char* argv[]) {
@@ -100,10 +146,13 @@ int main(int argc, char* argv[]) {
     printf("crepl> ");
     fflush(stdout);
     if (fgets(line, sizeof(line), stdin) != NULL) {
-      if (strncmp(line, "int", 3) == 0) rc = compile_shared_lib(line);
+      if (strncmp(line, "int", 3) == 0) compile_shared_lib(line);
       else rc = fetch_expression_value(line);
     }
-    if (is_expression) printf("= %d\n", rc);
+    if (is_expression) {
+      if (is_valid) printf("%d\n", rc);
+      else printf("Invalid expression.\n");
+    }
     else printf("Compile %s.\n", rc ? "ok" : "error");
   }
   return 0;
--- .shadow/test/source.c
+++
@@ -1,1 +0,0 @@
-int fuck() { return }

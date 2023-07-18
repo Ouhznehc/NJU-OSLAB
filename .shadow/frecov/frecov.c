97146defc23c936f20f7510e0d8a5738182f24bb
tracer-nju <tracer@nju.edu.cn>
2023-07-18 16:09:09 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  16:09:09 up  1:00,  1 user,  load average: 0.98, 0.70, 0.48

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -71,7 +71,8 @@ int main(int argc, char* argv[]) {
   unsigned char cluster[CLUS_SZ];
   for (u8* cluster_ptr = data_st; cluster_ptr < data_ed; cluster_ptr += cluster_sz) {
     memcpy(cluster, cluster_ptr, CLUS_SZ);
-    if (cluster[0] == 'B' && cluster[1] == 'M') printf("Found a BMP header!\n");
+    // if (cluster[0] == 'B' && cluster[1] == 'M') printf("Found a BMP header!\n");
+    if (*(u16*)cluster_ptr == 0x4d42) printf("Found a BMP header!\n");
     // else printf("this cluster not found!\n");
   }
 

7822ec2d0930aa35c9ac18334fd7dfe110cf270d
tracer-nju <tracer@nju.edu.cn>
2023-07-22 17:18:17 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  17:18:17 up  2:07,  1 user,  load average: 1.13, 0.53, 0.33

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -256,10 +256,6 @@ void get_long_filename(struct fat32Longdent* dent, int* clusId, char filename[])
   int cnt = 0;
   for (int i = ordinal - 1; i >= 0; i--) {
     struct fat32Longdent* Longdent = dent + i;
-    assert((Longdent->LDIR_Attr & ATTR_LONG_NAME) == ATTR_LONG_NAME);
-    assert(i == 0 || Longdent->LDIR_Ord == ordinal - i);
-
-
     for (int j = 0; j < 5; j++) filename[cnt++] = Longdent->LDIR_Name1[j];
     for (int j = 0; j < 6; j++) filename[cnt++] = Longdent->LDIR_Name2[j];
     for (int j = 0; j < 2; j++) filename[cnt++] = Longdent->LDIR_Name3[j];

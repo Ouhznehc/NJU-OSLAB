cc257bd3665dfbc3cd5545a66a7d9c1dbdea675f
tracer-nju <tracer@nju.edu.cn>
2023-07-24 16:43:02 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  16:43:02 up  5:59,  1 user,  load average: 0.79, 0.64, 0.41

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -194,7 +194,7 @@ int main(int argc, char* argv[]) {
       printf("%x %x\n", bmp_size, dent->DIR_FileSize);
       // assert(bmp_size == dent->DIR_FileSize);
       u8* bmp_st = (u8*)bmp_header;
-      printf("bmp_st = %x\n", bmp_st);
+      printf("bmp_st = %hhn\n", bmp_st);
       u8* bmp_ed = bmp_st + bmp_size;
       // printf("%x %x\n", (u32)bmp_st, (u32)bmp_ed);
       for (u8* bmp_ptr = bmp_st; bmp_ptr < bmp_ed; bmp_ptr++) fprintf(bmp, "%c", *bmp_ptr);

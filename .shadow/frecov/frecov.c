d850494efcf348a6531b01b802c322c8c64b1d51
tracer-nju <tracer@nju.edu.cn>
2023-07-24 16:00:30 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-45-generic #46~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jun 7 15:06:04 UTC 20 x86_64 x86_64 x86_64 GNU/Linux  16:00:30 up  5:17,  1 user,  load average: 0.96, 0.43, 0.24

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -194,7 +194,7 @@ int main(int argc, char* argv[]) {
       struct bmpHeader* bmp_header = cluster_to_sec(hdr, bmp_clus);
       u32 size = *(u32*)((u8*)bmp_header + 2);
       u32 bmp_size = bmp_header->bfSize;
-      printf("%x %x\n", bmp_size, dent->DIR_FileSize);
+      printf("%x %x %x\n", bmp_size, dent->DIR_FileSize, size);
       assert(bmp_size == dent->DIR_FileSize);
       u8* bmp_st = (u8*)bmp_header;
       u8* bmp_ed = bmp_st + bmp_size;

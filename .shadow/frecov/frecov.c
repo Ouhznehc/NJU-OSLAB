e963727997e1b9b41aa00bd40f08b68ebafd270f
tracer-nju <tracer@nju.edu.cn>
2023-07-25 21:40:05 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:40:05 up 30 min,  1 user,  load average: 0.74, 0.57, 0.54

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -228,8 +228,7 @@ int main(int argc, char* argv[]) {
           u8* next_clus_st = clus_to_sec(hdr, clus);
 
           //to accelerate(must)
-          // if (cur_pos >= bmp_width && *next_clus_st != 0) continue;
-          assert(next_clus_st + bmp_width - cur_pos < clus_ed && next_clus_st + bmp_width - cur_pos > clus_st);
+          if (cur_pos >= bmp_width && *next_clus_st != 0) continue;
           if (cur_pos < bmp_width && *(next_clus_st + bmp_width - cur_pos) != 0) continue;
 
           for (int k = 0; k < bmp_row && bmp_cnt + k < bmp_size; k++) {

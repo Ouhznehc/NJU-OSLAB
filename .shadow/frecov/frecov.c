bcfda36e3a62e098a523542bbbdc68e73411360c
tracer-nju <tracer@nju.edu.cn>
2023-07-25 22:06:39 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:06:39 up 57 min,  1 user,  load average: 0.40, 0.37, 0.28

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -219,7 +219,7 @@ int main(int argc, char* argv[]) {
 
         bmp_ptr = 0;
         u32 min_rgb = 0x5f5f5f5f;
-        u32 next_clus = -1;
+        u32 next_clus = 2;
 
         for (int clus = 2; clus < clus_cnt; clus++) {
           if (clus_type[clus] != CLUS_BMP_DATA) continue;

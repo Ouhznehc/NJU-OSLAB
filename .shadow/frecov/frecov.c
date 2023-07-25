451d9b70b9c44c81fff9af2dddb47349a0533311
tracer-nju <tracer@nju.edu.cn>
2023-07-25 15:08:04 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:08:04 up 10 min,  1 user,  load average: 0.33, 0.63, 0.49

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -189,9 +189,6 @@ int main(int argc, char* argv[]) {
       if (bmp_clus < 2 || bmp_clus >= CLUS_CNT || cluster_type[bmp_clus] != CLUS_BMP_HEAD) continue; //defensive
 
       sprintf(file_name, "/tmp/%s", bmp_name);
-      printf("e0827a916543e8e442611016ad6f9e97a864a929 %s\n", bmp_name);
-      continue;
-
       FILE* bmp = fopen(file_name, "w");
       struct bmpHeader* bmp_header = (struct bmpHeader*)cluster_to_sec(hdr, bmp_clus);
       u32 bmp_size = bmp_header->bfSize;
@@ -200,7 +197,7 @@ int main(int argc, char* argv[]) {
 
       u8* bmp_st = (u8*)bmp_header;
       u8* bmp_ed = bmp_st + bmp_size;
-      if (bmp_ed > data_ed || bmp_st < data_st) continue;
+      //if (bmp_ed > data_ed || bmp_st < data_st) continue;
 
       for (u8* bmp_ptr = bmp_st; bmp_ptr < bmp_ed; bmp_ptr++) fprintf(bmp, "%c", *bmp_ptr);
       fclose(bmp);

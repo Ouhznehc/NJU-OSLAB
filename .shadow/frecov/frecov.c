eeb9820a20c0c1004f8fdbccfa1c5bda1240c074
tracer-nju <tracer@nju.edu.cn>
2023-07-25 10:08:13 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:08:13 up 14 min,  1 user,  load average: 1.19, 0.81, 0.54

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -159,7 +159,7 @@ int main(int argc, char* argv[]) {
     int is_dir = cluster_type[cluster_id++] == CLUS_DENT;
     if (!is_dir) continue;
     int bmp_clus = 0;
-    char bmp_name[64], file_name[128];
+    char bmp_name[256], file_name[512];
 
     for (int d = 0; d < ndents; d++) {
       struct fat32dent* dent = (struct fat32dent*)cluster_ptr + d;

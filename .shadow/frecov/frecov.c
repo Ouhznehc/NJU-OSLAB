988911a51c351d7bf25069669a9abdd58a04d3cb
tracer-nju <tracer@nju.edu.cn>
2023-07-25 22:54:40 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:54:40 up  1:45,  1 user,  load average: 1.48, 0.58, 0.58

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -200,10 +200,10 @@ int main(int argc, char* argv[]) {
       u8* bmp_ed = bmp_st + bmp_size;
       if (bmp_ed > clus_ed || bmp_st < clus_st) continue;
 
-      u32 bmp_width = 3 * bmp_header->biWidth;
+      u32 bmp_width = bmp_header->biWidth;
       u32 bmp_offset = bmp_header->bfOffByte;
       u8* data_st = bmp_st + bmp_offset;
-      u32 bmp_row = (8 * bmp_width + 31) / 32 * 4; // 4 bytes align
+      u32 bmp_row = (3 * 8 * bmp_width + 31) / 32 * 4; // 4 bytes align
 
       for (u8* bmp_ptr = bmp_st; bmp_ptr < data_st; bmp_ptr++) fputc(*bmp_ptr, bmp);
 
@@ -228,8 +228,8 @@ int main(int argc, char* argv[]) {
           u8* next_clus_st = clus_to_sec(hdr, clus);
 
           //to accelerate(must)
-          if ((cur_pos < bmp_width) && (*(next_clus_st + bmp_width - cur_pos) != 0)) continue;
-          if ((cur_pos >= bmp_width) && (*next_clus_st != 0)) continue;
+          if ((cur_pos < 3 * bmp_width) && (*(next_clus_st + 3 * bmp_width - cur_pos) != 0)) continue;
+          if ((cur_pos >= 3 * bmp_width) && (*next_clus_st != 0)) continue;
 
           for (int k = 0; k < bmp_row && bmp_cnt + k < bmp_size; k++) {
             cur_rgb += rgb_distance(bmp_st + clus_sz - bmp_row + k, next_clus_st + k);

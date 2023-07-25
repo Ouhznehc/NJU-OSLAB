a45eefe3c255220177ec894391ba90b8490d74aa
tracer-nju <tracer@nju.edu.cn>
2023-07-25 22:20:19 +0800

    Linux ouhznehc-B660M-GAMING-AC-DDR4 5.19.0-50-generic #50-Ubuntu SMP PREEMPT_DYNAMIC Mon Jul 10 18:24:29 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:20:19 up  1:10,  1 user,  load average: 0.24, 0.22, 0.27

--- .shadow/frecov/frecov.c
+++ .shadow/frecov/frecov.c
@@ -218,14 +218,14 @@ int main(int argc, char* argv[]) {
         if (bmp_ptr != clus_sz) continue;
 
         bmp_ptr = 0;
-        u32 min_rgb = 0x5f5f5f5f;
-        u32 next_clus = 0;
+        u32 min_rgb = 0x3fffffff;
+        u32 next_clus = -1;
 
         for (int clus = 2; clus < clus_cnt; clus++) {
           if (clus_type[clus] != CLUS_BMP_DATA) continue;
 
           u32 cur_rgb = 0;
-          u8* next_clus_st = clus_to_sec(hdr, clus);
+          u8* next_clus_st = (u8*)clus_to_sec(hdr, clus);
 
           //to accelerate(must)
           if (cur_pos >= bmp_width && *next_clus_st != 0) continue;
@@ -236,12 +236,13 @@ int main(int argc, char* argv[]) {
             if (cur_rgb > min_rgb) break; // to accelerate(must)
           }
 
-          if (cur_rgb < min_rgb || min_rgb == 0x5f5f5f5f) {
+          if (cur_rgb < min_rgb) {
             next_clus = clus;
             min_rgb = cur_rgb;
           }
         }
 
+        assert(next_clus != -1);
         clus_type[next_clus] = CLUS_INVALID;
         cur_clus = next_clus;
         bmp_st = clus_to_sec(hdr, cur_clus);
